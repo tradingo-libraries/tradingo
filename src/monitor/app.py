dd165e99cc258b31d3976e79c0c9d8cdf8d2afb4
Rory McStay <rory@rorymcstay.com>
2024-11-05 18:20:02 +0000

    fix up tradingo monitor

--- src/monitor/app.py
+++ src/monitor/app.py
@@ -1,10 +1,12 @@
+import os
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 from tradingo.api import Tradingo
 
-ARCTIC_URL = (
-    "s3://s3.us-east-1.amazonaws.com:tradingo-store?aws_auth=true&path_prefix=prod"
+ARCTIC_URL = os.environ.get(
+    "TRADINGO_ARCTIC_URL",
+    "lmdb:///home/rory/dev/tradingo-plat/data/prod/tradingo.db",
 )
 
 pd.options.plotting.backend = "plotly"
@@ -24,30 +26,57 @@ app.layout = html.Div(
             options=api.instruments["im-multi-asset"]().index.to_list(),
             id="dropdown-selection",
         ),
-        dcc.Graph(id="graph-content"),
+        dcc.Graph(id="z_score"),
+        dcc.Graph(id="unrealised_pnl"),
+        dcc.Graph(id="month-to-date"),
     ]
 )
 
 
 @callback(
-    (Output("graph-content", "figure"),),
+    (
+        Output("z_score", "figure"),
+        Output("unrealised_pnl", "figure"),
+        Output("month-to-date", "figure"),
+    ),
     Input("dropdown-selection", "value"),
 )
 def update_graph(value):
     value = value or None
     z_score = api.signals.intraday_momentum.z_score(
         columns=value,
-        date_range=(pd.Timestamp.now("utc").normalize(), None),
+        date_range=(pd.Timestamp.now("utc").normalize() + pd.Timedelta(hours=9), None),
     )
 
     unrealised_pnl = api.backtest.intraday.rounded.position.instrument.unrealised_pnl(
         columns=value,
-        date_range=(pd.Timestamp.now("utc").normalize(), None),
+        date_range=(pd.Timestamp.now("utc").normalize() + pd.Timedelta(hours=13), None),
     )
-    return (z_score.plot(), unrealised_pnl.plot())
+
+    mtd = (
+        api.backtest.intraday.rounded.position.portfolio(
+            date_range=(
+                pd.Timestamp.now("utc").normalize() - pd.offsets.Day(30),
+                None,
+            ),
+        )
+        .resample(pd.offsets.BDay(1))
+        .last()
+        .diff()
+        .fillna(0)
+        .cumsum()
+        .total_pnl
+    )
+
+    return (
+        z_score.plot(title="z_score"),
+        unrealised_pnl.plot(title="unrealised_pnl"),
+        mtd.plot(title="Month to date returns"),
+    )
 
 
 if __name__ == "__main__":
     app.run(
+        host="0.0.0.0",
         debug=True,
     )
--- src/tradingo/signals.py
+++ src/tradingo/signals.py
@@ -175,6 +175,7 @@ def intraday_momentum(
     vol_floor_window=120,
     vol_floor_quantile=0.75,
     ffill_limit: int = 1,
+    start_after: int = 0,
     **kwargs,
 ):
 
@@ -250,6 +251,20 @@ def intraday_momentum(
     ).where(
         np.sign(signal.shift()).eq(np.sign(signal)), 0  # make changes in sign 0
     )
+
+    def get_pre_trade_index(periods):
+        idx = pd.DatetimeIndex([])
+
+        for i in range(0, periods):
+            idx = idx.union(
+                schedule.market_open + (pd.tseries.frequencies.to_offset(frequency) * i)
+            )
+
+        return pd.to_datetime(idx)
+
+    if start_after:
+        idx = get_pre_trade_index(start_after)
+        signal.loc[signal.index.isin(idx)] = 0.0
 
     def dynamic_floor(series, shift=0):
         return series.groupby(series.index.date).transform(
