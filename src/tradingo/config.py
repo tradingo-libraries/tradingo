27077636710667fc226dee80d87ee489eb502feb
Rory McStay <rory@rorymcstay.com>
2025-05-04 15:01:54 +0100

    fix up parsing

--- src/tradingo/config.py
+++ src/tradingo/config.py
@@ -2,7 +2,8 @@
 
 import json
 import pathlib
-from typing import Any
+from copy import copy
+from typing import Any, Optional
 
 import jinja2
 import yaml
@@ -12,46 +13,77 @@ class ConfigLoadError(Exception):
     """raised when loading config incorrectly."""
 
 
-def read_config_template(filepath: pathlib.Path, variables) -> dict[str, Any]:
+def read_config_template(
+    filepath: pathlib.Path,
+    variables: dict[str, Any],
+    path_so_far: Optional[list[str]] = None,
+) -> dict[str, Any]:
     """read a config template and populate it with values"""
     filepath = pathlib.Path(filepath)
 
+    path_so_far = path_so_far or []
+
+    # TODO:  undefined=jinja2.StrictUndefined
     renderedtext = jinja2.Template(filepath.read_text(encoding="utf-8")).render(
         **variables
     )
 
     try:
         if filepath.suffix == ".json":
-            return process_includes(json.loads(renderedtext), variables)
+            return process_includes(
+                json.loads(renderedtext),
+                variables,
+                path_so_far=path_so_far,
+            )
 
         if filepath.suffix == ".yaml":
-            return process_includes(yaml.safe_load(renderedtext), variables)
+            return process_includes(
+                yaml.safe_load(renderedtext),
+                variables,
+                path_so_far=path_so_far,
+            )
 
     except jinja2.TemplateSyntaxError as ex:
         raise ConfigLoadError(f"Error reading config template: {filepath}") from ex
+    except FileNotFoundError as ex:
+        raise ConfigLoadError(
+            f"Error rendering include: {filepath=} {path_so_far=}"
+        ) from ex
 
     raise ValueError(f"Unhandled file type: '{filepath.suffix}'")
 
 
-def process_includes(config: dict[str, Any], variables) -> dict[str, Any]:
+def process_includes(
+    config: dict[str, Any],
+    variables,
+    path_so_far: Optional[list[str]] = None,
+) -> dict[str, Any]:
     """bake nested configs via 'include' pattern."""
 
     out = {}
 
+    path_so_far = path_so_far or []
     for key, value in config.items():
+        path_so_far.append(key)
         if isinstance(value, dict) and "include" in value:
-            value = process_includes(value, variables)
+            value = process_includes(value, variables, path_so_far)
 
             protocol, path = value["include"].split("://")
 
             if protocol == "file":
-                incvalue = read_config_template(
-                    pathlib.Path(path),
-                    variables={
-                        **value.get("variables", {}),
-                        **variables,
-                    },
-                )
+                try:
+                    incvalue = read_config_template(
+                        pathlib.Path(path),
+                        variables={
+                            **value.get("variables", {}),
+                            **variables,
+                        },
+                        path_so_far=path_so_far,
+                    )
+                except ConfigLoadError as ex:
+                    raise ConfigLoadError(
+                        f"Error processing include at '{value}' {path_so_far=}"
+                    )
 
             else:
                 raise ValueError(
@@ -63,7 +95,7 @@ def process_includes(config: dict[str, Any], variables) -> dict[str, Any]:
             value.pop("variables", None)
 
         elif isinstance(value, dict):
-            value = process_includes(value, variables)
+            value = process_includes(value, variables, path_so_far)
 
         out[key] = value
 
