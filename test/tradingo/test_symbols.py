ea9aeebe6b919457326dbbceafc91aad9aad1942
Rory McStay <rory@rorymcstay.com>
2026-01-28 18:11:43 +0000

    add test for reading pickled data.

---
+++ test/tradingo/test_symbols.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import pytest
+from arcticdb import Arctic
+
+from tradingo.symbols import symbol_provider, symbol_publisher
+
+
+@pytest.fixture
+def arctic() -> Arctic:
+    return Arctic("mem://test-db")
+
+
+def test_symbol_provider(arctic: Arctic) -> None:
+
+    @symbol_provider(input_1="my-lib/symbol")
+    def provider(input_1: pd.DataFrame) -> pd.DataFrame:
+        return input_1
+
+    @symbol_publisher("my-lib/symbol")
+    def publisher(input_1: pd.DataFrame) -> pd.DataFrame:
+        return pd.DataFrame(
+            1,
+            index=pd.date_range("2025-01-01", "2026-01-01"),
+            columns=["A"],
+        )
+
+    publisher(arctic=arctic, input_1=pd.DataFrame(), dry_run=False)
+    res = provider(arctic=arctic, input_1="my-lib/symbol")  # type: ignore
+
+    assert res["A"].mean() == 1
+
+
+def test_symbols_function_is_provided_for_and_published(arctic: Arctic) -> None:
+
+    data = pd.DataFrame(
+        1,
+        index=pd.date_range("2026-01-27", "2026-01-28"),
+        columns=["A"],
+    )
+
+    @symbol_provider(input_1="my-lib/symbol")  # type: ignore
+    @symbol_publisher("my-lib/out-symbol")
+    def provider_publisher(input_1: pd.DataFrame) -> pd.DataFrame:
+        return input_1
+
+    lib = arctic.get_library(name="my-lib", create_if_missing=True)
+    lib.write("symbol", data)
+
+    provider_publisher(arctic=arctic, input_1="my-lib/symbol", dry_run=False)  # type: ignore
+
+    assert lib.list_symbols(regex="out-symbol")
+
+
+def test_symbols_provider_fallsback_to_pickle(arctic: Arctic) -> None:
+
+    data = pd.DataFrame(
+        1,
+        index=pd.date_range("2026-01-27", "2026-01-28"),
+        columns=["A"],
+    )
+
+    @symbol_provider(input_1="my-lib/symbol")  # type: ignore
+    @symbol_publisher("my-lib/out-symbol")
+    def provider_publisher(input_1: pd.DataFrame) -> pd.DataFrame:
+        return input_1
+
+    lib = arctic.get_library(name="my-lib", create_if_missing=True)
+    lib.write_pickle("symbol", data)
+
+    provider_publisher(arctic=arctic, input_1="my-lib/symbol", dry_run=False)  # type: ignore
+
+    assert lib.list_symbols(regex="out-symbol")
